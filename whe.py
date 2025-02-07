e3f0aa983c5cfcf00ccaee504fff3bea344c4bb3
darya-k00 <dasha.kon.060203@gmail.com>
2025-02-07 16:49:50 +0300

    Update whe.py

--- whe.py
+++ whe.py
@@ -1,9 +1,20 @@
 import requests
+from urllib.parse import urlencode
 
 def get_weather(city):
-    url = f'https://wttr.in/{city}?MnTq&lang=ru'
+    params = {
+        'MnTq': '',
+        'lang': 'ru'
+    }
+    query_string = urlencode(params)
+    url = f'https://wttr.in/{city}?{query_string}'
     response = requests.get(url)
-    return response.text
+    response.raise_for_status()
+
+    if response.status_code==200:
+        return response.text
+    else:
+        print(f"{response.status_code}")
 
 if __name__ == "__main__":
     cities = ["аэропорт Шереметьево", "Лондон", "Череповец"]
