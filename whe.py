c10d041623058c3203ec204e1c2f462fef3359e3
darya-k00 <dasha.kon.060203@gmail.com>
2025-02-12 16:56:35 +0300

    Update whe.py

--- whe.py
+++ whe.py
@@ -6,17 +6,19 @@ def get_weather(city):
         'lang': 'ru'
     }
     url = f'https://wttr.in/{city}'
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    return response.text
 
+def main():
+    cities = ["аэропорт Шереметьево", "Лондон", "Череповец"]
     try:
-        response = requests.get(url, params=params)
-        response.raise_for_status()
-        return response.text
+        for city in cities:
+            weather_info = get_weather(city)
+            print(weather_info)
     except requests.exceptions.RequestException as err:
         return None
 
 if __name__ == "__main__":
-    cities = ["аэропорт Шереметьево", "Лондон", "Череповец"]
+    main()
 
-    for city in cities:
-        weather_info = get_weather(city)
-        print(weather_info)
